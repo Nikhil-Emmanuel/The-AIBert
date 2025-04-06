b9717f4219f4d7c5c22b8995e01bffc0b5c19a17
T Nikhil Emmanuel <99235919+Nikhil-Emmanuel@users.noreply.github.com>
2025-04-06 22:06:32 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
     host: true, // exposes server to network
     port: 5173,
     allowedHosts: [
-      'ec2-98-84-179-217.compute-1.amazonaws.com' // replace with your EC2 DNS
+      'ec2-98-84-179-217.compute-1.amazonaws.com','the-aibert.duckdns.org' // replace with your EC2 DNS
     ]
   }
 })
